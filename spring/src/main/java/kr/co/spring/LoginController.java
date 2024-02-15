8267fd4a7f81023e696e3a9c8a3d1cd08dc384c9
jiho-96 <yujh9610@gmail.com>
2024-02-16 01:25:42 +0900

    session study

--- spring/src/main/java/kr/co/spring/LoginController.java
+++ spring/src/main/java/kr/co/spring/LoginController.java
@@ -1,12 +1,15 @@
 package kr.co.spring;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
@@ -17,9 +20,16 @@ public class LoginController {
     public String loginForm(){
         return "loginForm";
     }
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
+        // 1. 세션 종료
+        session.invalidate();
+        // 2. 홈으로 이동
+        return "redirect:/";
+    }
 
     @PostMapping("/login")
-    public String login(String id, String pwd, boolean rememberId, HttpServletResponse response) throws Exception {
+    public String login(/*@CookieValue("JSESSIONID") String sessionId,*/ String id, String pwd, String toURL, boolean rememberId, HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("id=" + id);
         System.out.println("pwd=" + pwd);
         System.out.println("rememberId=" + rememberId);
@@ -31,6 +41,10 @@ public class LoginController {
             return "redirect:/login/login?msg=" + msg;
         }
         // id와 pwd가 일치하면
+        // 세션 객체를 얻어오기
+        HttpSession session = request.getSession();
+        // 세션 객체에 id를 저장
+        session.setAttribute("id", id);
 
         // 체크박스를 체크하면
         if (rememberId){
@@ -45,8 +59,11 @@ public class LoginController {
         }
 
         //홈으로 이동
-        return "redirect:/";
+
+        toURL = toURL == null || toURL.equals("") ? "/" : toURL; // toURL이 있으면 toURL로 이동
+        return "redirect:" + toURL;
     }
+
 
     private boolean  logCheck(String id, String pwd){
         return "asdf".equals(id) && "1234".equals(pwd);
